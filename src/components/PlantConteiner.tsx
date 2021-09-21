24455ac60471cd1412789e9b94539272619155e2
pinomartin <martin91pino@gmail.com>
2021-09-21 02:10:15 -0300

    imgs of plants

---
+++ src/components/PlantConteiner.tsx
@@ -0,0 +1,13 @@
+import { Row, Col } from "react-bootstrap"
+
+const PlantConteiner = () => {
+    return (
+        <Row>
+            <Col xs={12}>
+
+            </Col>
+        </Row>
+    )
+}
+
+export default PlantConteiner
---
+++ src/screens/PlantsScreen.tsx
@@ -0,0 +1,10 @@
+
+const PlantsScreen = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default PlantsScreen
